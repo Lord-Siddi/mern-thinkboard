a3feab8b89277a4b2cb4e677ff50bae84b17eea1
Lord-Siddi <sidjindal008@gmail.com>
2025-09-07 18:22:47 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,35 +1,47 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
+
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
-import ratelimiter from "./middleware/rateLimiter.js";
-import cors from "cors";
+import rateLimiter from "./middleware/rateLimiter.js";
+
 dotenv.config();
 
-const app=express();
-const PORT=process.env.PORT||5001;
+const app = express();
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-app.use(cors({
-    origin: "http://localhost:5173",
-})
-);
-//middleware
-app.use(express.json());//this middleware will parse JSON bodies: req.body
-app.use(ratelimiter)
+// middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
+app.use(rateLimiter);
 
-//our simple custom middleware
-// app.use((req,res,next)=>{
-//     console.log(`Req method is &{req.method} & Req URL is ${req.url}`);
-//     next();
+// our simple custom middleware
+// app.use((req, res, next) => {
+//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//   next();
 // });
+
 app.use("/api/notes", notesRoutes);
 
-//What is an Endpoint?
-//An endpoint is a combination of a URL+HTTP method that lets the client
-//interact with a specific resource.
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-connectDB().then(()=>{
-    app.listen(PORT, ()=>{
-        console.log("Server started on PORT:", PORT)
-    });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
 });
